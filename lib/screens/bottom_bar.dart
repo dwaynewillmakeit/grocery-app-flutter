ea50eb835f31e79f1bc0c89834a641e96c441d2f
Dwayne Williams <dwaynewillmakeit@gmail.com>
2023-09-13 01:22:31 -0500

    Implemented Top App Bar

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -17,11 +17,12 @@ class BottomBarScreen extends StatefulWidget {
 class _BottomBarScreenState extends State<BottomBarScreen> {
   int _selectedIndex = 0;
 
-  final List _pages = [
-    const HomeScreen(),
-    const CategoriesScreen(),
-    const UserScreen(),
-    const CartScreen()
+  final List<Map<String, dynamic>> _pages = [
+    {"page":const HomeScreen(),'title': 'Home Screen'},
+    {"page":const CategoriesScreen(),'title': 'Categories Screen'},
+    {"page":const CartScreen(),'title': 'Cart Screen'},
+    {"page":const UserScreen(),'title': 'User Screen'},
+
   ];
 
   void _selectedPage(int index) {
@@ -35,7 +36,10 @@ class _BottomBarScreenState extends State<BottomBarScreen> {
     final themeState = Provider.of<DarkThemeProvider>(context);
     bool isDark = themeState.getDarkTheme;
     return Scaffold(
-      body: _pages[_selectedIndex],
+      appBar: AppBar(
+        title: Text(_pages[_selectedIndex]['title']),
+      ),
+      body: _pages[_selectedIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: isDark? Theme.of(context).cardColor:Colors.white ,
         type: BottomNavigationBarType.fixed,
